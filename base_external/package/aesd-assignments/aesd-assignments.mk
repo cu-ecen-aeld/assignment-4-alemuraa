08c535c79e28e1ff8842f49cee92f29b6e548492
alemuraa <alemuraa@UbuntuMura.myguest.virtualbox.org>
2025-06-19 04:41:07 -0600

    Fix: use github download method for aesd-assignments

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,33 +1,17 @@
+################################################################################
+# aesd-assignments.mk - Buildroot package file for assignments-3-and-later
+################################################################################
 
-##############################################################
-#
-# AESD-ASSIGNMENTS
-#
-##############################################################
-
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = 665cec9d75da71b02753f0316accc2f27c0f7b5d
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-alemuraa.git
+AESD_ASSIGNMENTS_SITE = $(call github,cu-ecen-aeld,assignments-3-and-later-alemuraa,$(AESD_ASSIGNMENTS_VERSION))
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
-define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
-endef
-
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0775 $(@D)/server/aesdsocket $(TARGET_DIR)/bin
-	$(INSTALL) -m 0775 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
-	$(INSTALL) -d 0775 $(@D)/finder-app/ $(TARGET_DIR)/bin
-	$(INSTALL) -m 0775 $(@D)/finder-app/* $(TARGET_DIR)/bin
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
+
